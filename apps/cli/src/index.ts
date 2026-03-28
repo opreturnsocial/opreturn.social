914e3ae04793eaec6a29f3ffe4866bcd1e2fcb18
Roland Bewick <roland.bewick@gmail.com>
2026-03-28 23:48:39 +0700

    chore: update cli and protocol package versions

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -25,7 +25,7 @@ program
       "    2. Config file (~/.ors/cli/config.json)\n\n" +
       "  Run '@opreturnsocial/cli setup --generate' to get started.",
   )
-  .version("0.2.0");
+  .version("0.2.1");
 
 registerSetupCommand(program);
 registerWhoamiCommand(program);
